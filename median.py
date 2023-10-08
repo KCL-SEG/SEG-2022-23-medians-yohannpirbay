0f26cc885d628aaa7746b4f3c288dfbf52373912
Yohann Pirbay <pirbay.yohann@gmail.com>
2023-10-09 00:39:30 +0100

    First Commit

--- median.py
+++ median.py
@@ -9,4 +9,11 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+if (len(numbers)%2 == 1): 
+    mid = len(numbers) // 2
+    print(numbers[mid])
+else:
+    mid = len(numbers) // 2 - 1
+    print((numbers[mid] + numbers[mid+1]) / 2)
